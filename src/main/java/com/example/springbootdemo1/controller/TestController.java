b2b47f4c15f2a3320e78317e91d61a0d65db9fc2
xx <buptxuxin@163.com>
2020-11-17 15:29:27 +0800

    修改返回内容

--- src/main/java/com/example/springbootdemo1/controller/TestController.java
+++ src/main/java/com/example/springbootdemo1/controller/TestController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/test")
     public String test(){
-      return "success";
+
+      return "failure is mama of success";
     }
 
 }
