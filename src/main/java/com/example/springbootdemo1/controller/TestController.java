e8e4b7cafe298ca27423782355610c5ddcfc3ab7
xx <buptxuxin@163.com>
2020-11-18 10:10:29 +0800

    修改为restful风格的接口

--- src/main/java/com/example/springbootdemo1/controller/TestController.java
+++ src/main/java/com/example/springbootdemo1/controller/TestController.java
@@ -1,16 +1,34 @@
 package com.example.springbootdemo1.controller;
 
+import com.example.springbootdemo1.entity.Book;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequestMapping("/book")
 @RestController
 public class TestController {
-    @RequestMapping("/test")
-    public String test(){
-      System.out.println("remember this");
-      return "failure is mama of success";
+    List<Book>  books=new ArrayList<>();
+    @RequestMapping("/add")
+    public ResponseEntity test(@RequestBody Book book){
+        System.out.println(book);
+      books.add(book);
+      return ResponseEntity.ok(books);
     }
 
+    @RequestMapping("/delete/{id}")
+    public ResponseEntity test2(@PathVariable("id") int id){
+        books.remove(id);
+        return ResponseEntity.ok(books);
+    }
+
+    @RequestMapping("/get")
+    public ResponseEntity test3(@RequestParam("name") String name){
+        List<Book> results=books.stream().filter(book -> book.getName().equals(name)).collect(Collectors.toList());
+        return ResponseEntity.ok(books);
+    }
 }
